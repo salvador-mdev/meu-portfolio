a533ce33441cf89a9a05e65655dc7da6ebefc196
Salvador Montero <salvadormdev@gmail.com>
2024-08-13 16:28:43 -0400

    agregando animaciones con scrollreveal

---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+  sr.reveal('.informacoes', {
+    duration: 3000,
+    origin: 'rigth',
+    distance: '-100px'
+  });
+
+  sr.reveal('.container-foto', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+
+  sr.reveal('.p1', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-200px'
+  });
+
+  sr.reveal('.p2', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.p3', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-400px'
+  });
+
+  sr.reveal('.p4', {
+    duration: 4500,
+    origin: 'top',
+    distance: '-500px'
+  });
+
+  sr.reveal('.p5', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.p6', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-300px'
+  });
+
+  sr.reveal('.p7', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-300px'
+  });
